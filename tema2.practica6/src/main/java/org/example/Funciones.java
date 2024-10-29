5dedd48c4b36ee8246e39f59e770add23f6f861e
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-10-29 14:05:57 +0100

    commit2

--- tema2.practica6/src/main/java/org/example/Funciones.java
+++ tema2.practica6/src/main/java/org/example/Funciones.java
@@ -79,14 +79,16 @@ public class Funciones {
     public static void consultaEstudiantesSinMascotas(Connection Conexion) {
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
+            String consultaSQL = "SELECT estudiantes.nombre_completo FROM estudiantes LEFT JOIN mascotas ON estudiantes.id = mascotas.estudiante_id WHERE mascotas.id IS NULL;";
             PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
 
             ResultSet resultados = consulta.executeQuery();
 
             while (resultados.next()) {
 
+                String nombre = resultados.getString("nombre_completo");
 
+                System.out.println("Nombre : " + nombre);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -96,13 +98,16 @@ public class Funciones {
     public static void consultaPromedioNotasPorEstudiante(Connection Conexion) {
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
+            String consultaSQL = "SELECT estudiantes.nombre_completo, AVG(calificaciones.nota) AS promedio FROM estudiantes JOIN calificaciones ON estudiantes.id = calificaciones.estudiante_id GROUP BY estudiantes.nombre_completo;";
             PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
 
             ResultSet resultados = consulta.executeQuery();
 
             while (resultados.next()) {
 
+                String nombre = resultados.getString("nombre_completo");
+                double promedio = resultados.getDouble("promedio");
+                System.out.println("Nombre : " + nombre + " Promedio: " + promedio);
 
             }
         } catch (SQLException e) {
@@ -113,13 +118,16 @@ public class Funciones {
     public static void consultaEstudiantesQuintoAnoPorCasa(Connection Conexion) {
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
+            String consultaSQL = "SELECT casas.nombre, COUNT(estudiantes.id) AS num_estudiantes FROM estudiantes JOIN casas ON estudiantes.casa_id = casas.id WHERE estudiantes.año = 5 GROUP BY casas.nombre;";
             PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
 
             ResultSet resultados = consulta.executeQuery();
 
             while (resultados.next()) {
 
+                String nombre = resultados.getString("nombre");
+                int numEstudiantes = resultados.getInt("num_estudiantes");
+                System.out.println("Casa: " + nombre + " Num. Estudiantes: " + numEstudiantes);
 
             }
         } catch (SQLException e) {
@@ -132,13 +140,16 @@ public class Funciones {
         String asignatura = scanner.nextLine();
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
+            String consultaSQL = "SELECT estudiantes.nombre_completo, calificaciones.nota FROM estudiantes JOIN calificaciones ON estudiantes.id = calificaciones.estudiante_id JOIN asignaturas ON calificaciones.asignatura_id = asignaturas.id WHERE asignaturas.nombre =" + asignatura +  " ORDER BY calificaciones.nota DESC LIMIT 1;";
             PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
 
             ResultSet resultados = consulta.executeQuery();
 
             while (resultados.next()) {
 
+                String nombre = resultados.getString("nombre_completo");
+                double nota = resultados.getDouble("nota");
+                System.out.println("Nombre : " + nombre + " Nota: " + nota);
 
             }
         } catch (SQLException e) {
@@ -151,13 +162,16 @@ public class Funciones {
         String asignatura = scanner.nextLine();
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
+            String consultaSQL = "SELECT casas.nombre, AVG(calificaciones.nota) AS promedio FROM calificaciones JOIN estudiantes ON calificaciones.estudiante_id = estudiantes.id JOIN casas ON estudiantes.casa_id = casas.id JOIN asignaturas ON calificaciones.asignatura_id = asignaturas.id WHERE asignaturas.nombre =" +asignatura +"GROUP BY casas.nombre;";
             PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
 
             ResultSet resultados = consulta.executeQuery();
 
             while (resultados.next()) {
 
+                String nombre = resultados.getString("nombre");
+                double promedio = resultados.getDouble("promedio");
+                System.out.println("Casa: " + nombre + " Promedio: " + promedio);
 
             }
         } catch (SQLException e) {
@@ -168,15 +182,24 @@ public class Funciones {
     public static void incrementarCalificacionesUltimoAno(Connection Conexion) {
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
-            PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
+            String consultaSQL = "UPDATE calificaciones SET nota = nota * 1.1 WHERE estudiante_id IN ( SELECT id FROM estudiantes WHERE año = (SELECT MAX(año) FROM estudiantes));";
+            try {
 
-            ResultSet resultados = consulta.executeQuery();
+                PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
+                int rowsUpdate = consulta.executeUpdate();
 
-            while (resultados.next()) {
+                System.out.println("Se han actualizado " + rowsUpdate + " registros");
+            } catch (Exception e) {
 
-
+                try {
+                    // Deshacemos las operaciones realizadas en la base de datos
+                    Conexion.rollback();
+                    System.err.println("ROLLBACK ejecutado");
+                } catch (SQLException ex2) {
+                    System.err.println("Error haciendo ROLLBACK");
+                }
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -185,15 +208,24 @@ public class Funciones {
     public static void desmatricularEstudiantesOptativas(Connection Conexion) {
         try {
             Conexion.setAutoCommit(false);
-            String consultaSQL = "";
-            PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
+            String consultaSQL = "DELETE FROM calificaciones WHERE asignatura_id IN (SELECT id FROM asignaturas WHERE tipo = 'Optativa') AND nota < 5;";
+            try {
 
-            ResultSet resultados = consulta.executeQuery();
+                PreparedStatement consulta = Conexion.prepareStatement(consultaSQL);
+                int rowsUpdate = consulta.executeUpdate();
 
-            while (resultados.next()) {
-
+                System.out.println("Se han actualizado " + rowsUpdate + " registros");
+            } catch (Exception e) {
+                try {
+                    // Deshacemos las operaciones realizadas en la base de datos
+                    Conexion.rollback();
+                    System.err.println("ROLLBACK ejecutado");
+                } catch (SQLException ex2) {
+                    System.err.println("Error haciendo ROLLBACK");
+                }
 
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
