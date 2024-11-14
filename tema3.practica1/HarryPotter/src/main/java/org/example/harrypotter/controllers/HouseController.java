4f21d16163c9b89c756bcc5e2b729627eaff6f2e
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-11-14 09:33:17 +0100

    commit2

--- tema3.practica1/HarryPotter/src/main/java/org/example/harrypotter/controllers/HouseController.java
+++ tema3.practica1/HarryPotter/src/main/java/org/example/harrypotter/controllers/HouseController.java
@@ -1,4 +1,24 @@
 package org.example.harrypotter.controllers;
 
+import lombok.Getter;
+import org.example.entities.Author;
+import org.example.thymeleafexample.entities.Book;
+import org.example.thymeleafexample.repositories.BookRepository;
+import org.example.thymeleafexample.services.BookService;
+import org.example.thymeleafexample.services.BookServiceImplemenation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class HouseController {
+    @GetMapping("/book/{isbn}")
+    public String showBook(@PathVariable String name, Model model) {
+        model.addAttribute("house", houseService.getHouseByName(name));
+        return "house";
 }
