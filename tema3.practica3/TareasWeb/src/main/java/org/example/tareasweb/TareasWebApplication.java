313407ace523b1f58b025f6f758a9742c0a92290
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-12-03 12:44:55 +0100

    commit2

--- tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/TareasWebApplication.java
+++ tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/TareasWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompleteAppApplication {
+public class TareasWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CompleteAppApplication.class, args);
+        SpringApplication.run(TareasWebApplication.class, args);
     }
 
 }
