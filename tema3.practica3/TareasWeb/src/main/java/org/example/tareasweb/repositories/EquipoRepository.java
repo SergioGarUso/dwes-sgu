3be132f55c1870d7474b4ebb54dfc8938829eb0e
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-12-05 08:58:40 +0100

    commit2

--- tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/repositories/EquipoRepository.java
+++ tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/repositories/EquipoRepository.java
@@ -3,8 +3,8 @@ package org.example.tareasweb.repositories;
 import org.example.tareasweb.entities.Equipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
-public interface EquipoRepository extends JpaRepository<Equipo, Integer> {
+public interface EquipoRepository extends JpaRepository<Equipo, Long> {
+
 }
--- tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/repositories/TrabajadorRepository.java
+++ tema3.practica3/TareasWeb/src/main/java/org/example/tareasweb/repositories/TrabajadorRepository.java
@@ -3,8 +3,8 @@ package org.example.tareasweb.repositories;
 import org.example.tareasweb.entities.Trabajador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
-public interface TrabajadorRepository extends JpaRepository<Trabajador, Integer> {
+public interface TrabajadorRepository extends JpaRepository<Trabajador, Long> {
+
 }
